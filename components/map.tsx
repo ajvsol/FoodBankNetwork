0700a91a3c656298bd431b4cdf256a50f5e2e487
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-01-12 14:54:03 +0000

    map

--- components/map.tsx
+++ components/map.tsx
@@ -9,8 +9,6 @@ return(
   width="600"
   height="450"
   loading="lazy"
-  allowfullscreen
-  referrerpolicy="no-referrer-when-downgrade"
   src={`https://www.google.com/maps/embed/v1/place?key=${key}&q=${coord}`}>
 </iframe>
 </>)
