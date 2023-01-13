6f58d14ed8ddd880f1e4f9f8b2d6c889e347aa51
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-01-13 10:00:07 +0000

    style(CSS): Basic styling for homepage and navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { NavBar } from "../../components/NavBar";
 import { AboutComponent } from "../../components/aboutComponent";
+
 export default function About() {
   return(
   <div>
