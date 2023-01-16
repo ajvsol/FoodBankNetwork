bd798e6f187272690d503f40698edd0585af5b28
BenPuusta <benpuusta@gmail.com>
2023-01-16 15:19:29 +0000

    style:homepage dekstop layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function HomePage() {
 	return (
 		<div className='flex flex-col dark:bg-green-400 bg-green-400 h-screen min-h-screen'>
 			<NavBarHome/>
-			<div className='bg-red-500 flex flex-col justify-center items-center gap-5 flex-1'>
+			<div className='bg-red-500 flex flex-col margin items-center gap-5 flex-1'>
 				<a href='/'>
 					<Image
 						src="/../images/foodbanklogo2.svg"
 						alt="logo-image"
 						width="300"
 						height="200"
-						className='flex justify-center'
+						className='flex mt-20'
 					/>
 				</a>
 				<p>Welcome to our site</p>
