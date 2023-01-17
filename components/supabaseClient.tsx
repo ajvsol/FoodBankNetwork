cd371e514f3c41088c37d18315dded16122510d9
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-01-17 16:53:12 +0000

    env fixed

--- components/supabaseClient.tsx
+++ components/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ypecfggppeohabjrntsj.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZWNmZ2dwcGVvaGFianJudHNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM4NzYwMzIsImV4cCI6MTk4OTQ1MjAzMn0.apt0qpAftIzwDGRUnKDXsUQxfACNcWi8VohnBEB4Fz0"
+let supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ||"env bug"
+let supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY ||"env bug"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
