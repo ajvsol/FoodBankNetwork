17ee734d823608413f7de303cf88943ab7d42534
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-01-20 20:10:36 +0000

    wip1

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,18 +3,23 @@ import Image from "next/image";
 
 export function NavBar() {
   return (
-    <nav className="p-3 border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="p-3 border-gray-200 rounded bg-gray-100  dark:bg-gray-800 dark:border-gray-700">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <Link href="#" className="flex items-center">
-            <Image
+        <Link href="#" className="flex items-center ">
+            <img
               src="/logo2.png"
-              className="h-6 mr-3 sm:h-10"
+              className="h-6 sm:h-10 sm:w-36 lg:w-36"
               alt="logo-image"
-              width="150"
-              height="150"
-            />
+            ></img>
+            {/*<Image
+              src="/logo2.png"
+              className="h-6 sm:h-10 sm:w-36 lg:w-36"
+              alt="logo-image"
+
+              fill={true}
+            />*/}
         </Link>
-        <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+        <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex flex-row-reverse items-center  ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </button>
--- pages/find/index.tsx
+++ pages/find/index.tsx
@@ -26,6 +26,8 @@ export default function About() {
     setBank,
     comments,
     setComments,
+    toggle,
+    setToggle
   ]: any = useSearchContext();
   const router = useRouter();
 
@@ -51,6 +53,15 @@ export default function About() {
     console.log(`handleCard: `, location);
   }
 
+  function handleToggle() {
+    if (toggle == "hidden") {
+      setToggle("")
+     } else {
+      setToggle("hidden")
+    }
+    console.log ("toggle:", toggle)
+  }
+
   return (
     <div id="everything" className='dark:bg-gray-900'>
       <div className="p-3">
@@ -59,7 +70,22 @@ export default function About() {
       </div>
       {/* <FindMain/> */}
       <div id="mobile-content" className=" lg:hidden md:flex-col">
-        <p>toggle bar</p>
+        <label className="relative inline-flex items-center sm:hidden cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            className="sr-only peer"
+            onClick={() => {
+              handleToggle();
+            }}
+          />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Toggle me
+          </span>
+        </label>
+
+        <p className={`${toggle}`}>OOOOOOOOOOOOOOOOOOO</p>
         <Map coord={location} />
         <div
           id="List"
@@ -71,8 +97,6 @@ export default function About() {
               <Card
                 variant="bordered"
                 isPressable
-                //color='black' does't work, try CSS
-                //className='bg-black'
                 key={uuidv4()}
                 onPress={() => {
                   handleCard(index);
