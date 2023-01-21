3fad6471d450cc17e99ebc0eee2c733dbb0edaed
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-01-21 19:41:09 +0000

    style(more info page): styling much improved

--- pages/moreInfoBank/index.tsx
+++ pages/moreInfoBank/index.tsx
@@ -27,7 +27,9 @@ export default function MoreInfoBank() {
     setMapCode,
     mapCode,
   ]: any = useSearchContext();
+
   const router = useRouter();
+  const element = bank;
 
   function goBack() {
     router.push("/find");
@@ -49,41 +51,89 @@ export default function MoreInfoBank() {
       <Button onClick={goBack} color="warning" auto className="ml-3">
         Go Back
       </Button>
-      <Card
-        variant="bordered"
-        color="black"
-        className="bg-black"
-        key={uuidv4()}
-      >
-        <Card.Header>
-          <Text b>{bank.name}</Text>
-        </Card.Header>
-        <Card.Divider />
-        <Text className="ml-3 mr-3">{bank.address}</Text>
-        <Card.Body>
-          <p>{bank.address}</p>
-          <p>{bank.phone}</p>
-          <p>{bank.email}</p>
-        </Card.Body>
-        <Card.Divider />
-        <Card.Footer></Card.Footer>
-      </Card>
-      <h1 className="ml-3">Comments</h1>
-      <div className="ml-3">
+      <div 
+        id="mobile-content" 
+        className="md:hidden lg:hidden md:flex-col">
+        <Map coord={location} />
+        <div
+          id="List"
+          className="
+          ">
+          <Card
+            variant="bordered"
+            key={uuidv4()}
+            >
+            <Card.Header>
+              <Text b>{element.name}</Text>
+            </Card.Header>
+            <Card.Body>
+              <Text>{element.address}</Text>
+              <Text>{element.phone}</Text>
+              <Text>{element.email}</Text>
+              {/*<p>{bank.address}</p>
+              <p>{bank.phone}</p>
+              <p>{bank.email}</p>*/}
+            </Card.Body>
+            <Card.Divider />
+            <Card.Footer>
+              <Button size="sm" onClick={handleClick}>Directions</Button>
+            </Card.Footer>
+          </Card>
+        </div>
+      </div>
+      <div
+        id="desktop-content"
+        className="hidden md:flex lg:flex flex-row justify-items-start min-h-[80vh] max-h-[72vh]  lg:visible  ">
+        <Map coord={location} />
+        <div
+          id="List"
+          className="
+          min-w-[33%] max-w-[33%] overflow-auto my-3 pr-3
+          ">
+          <Card
+            variant="bordered"
+            key={uuidv4()}
+            >
+            <Card.Header>
+              <Text b>{element.name}</Text>
+            </Card.Header>
+            <Card.Body>
+              <Text>{element.address}</Text>
+              <Text>{element.phone}</Text>
+              <Text>{element.email}</Text>
+              {/*<p>{bank.address}</p>
+              <p>{bank.phone}</p>
+              <p>{bank.email}</p>*/}
+            </Card.Body>
+            <Card.Divider />
+            <Card.Footer>
+              <Button size="sm" onClick={handleClick}>Directions</Button>
+            </Card.Footer>
+          </Card>
+        </div>
+      </div>
+      <h1 className="ml-3 text-gray-900 text-xl p-2 dark:text-gray-300">Comments</h1>
+      <div className="ml-3 text-gray-900 dark:text-gray-300">
         {comments.map((element: any) => {
           return (
-            <div key={uuidv4()}>
-              <p>{element.comment}</p>
-              <p>{element.author}</p>
-            </div>
+            <Card
+              variant="bordered"
+              key={uuidv4()}
+              >
+              <Card.Header>
+                <Text b>{element.author}</Text>
+              </Card.Header>
+              <Card.Divider />
+              <Card.Body>
+                <Text>{element.comment}</Text>
+                {/*<p>{bank.address}</p>
+                <p>{bank.phone}</p>
+                <p>{bank.email}</p>*/}
+              </Card.Body>
+
+            </Card>
           );
         })}
-      </div>
-
-      <div>
-        <Map coord={location} origin={search} />
-
-        <button onClick={handleClick}>Directions</button>
       </div>
     </>
   );
