d726307798b75082a570b40e2b78441625604bb7
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-01-23 16:42:56 +0000

    more

--- components/aboutComponent.tsx
+++ components/aboutComponent.tsx
@@ -5,7 +5,7 @@ export function AboutComponent() {
 <p>We provide access to a national network of food banks to provide support to those who are struggling with food insecurity and poverty, we offer an up to date and easily accessible service to improve peoples current situations when they are looking for support.</p>
 
 <p>Our Food Bank Network is an independent support navigator that relies upon individuals, social support agencies, GPs, schools and similar organisations to update local information in order to help us with the mission of finding people their nearest food bank. In our searchable network we have the locations of over 2000 food bank centres.</p>
-<p>Our motivation is to bridge a gap in the structural support offered to the 15.5% of the UK population currently living with food insecurity. This means they ate less or went a day without eating because they couldn't access or afford food. In addition to this 10 million adults and 4 million children are living in poverty, they are the demographics that would mostly benefit from our network.
+<p>Our motivation is to bridge a gap in the structural support offered to the 15.5% of the UK population currently living with food insecurity. This means they ate less or went a day without eating because they couldnt access or afford food. In addition to this 10 million adults and 4 million children are living in poverty, they are the demographics that would mostly benefit from our network.
 Contact Us
 Email: info@foodbanknetwork
 </p>
